a749ae09244b4aa0d368186d4e9696545df3d59f
Alfonso Balbuena Sánchez <alfonso1011@gmail.com>
2020-12-19 01:54:39 -0600

    Add Fragments

---
+++ Project/app/src/main/java/com/alfonso/capstone/fragments/DetailPlaceFragment.java
@@ -0,0 +1,29 @@
+package com.alfonso.capstone.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.alfonso.capstone.R;
+
+public class DetailPlaceFragment extends Fragment {
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_detail_place, container, false);
+    }
+}
--- Project/app/src/main/java/com/alfonso/capstone/fragments/MapsLocationFragment.java
+++ Project/app/src/main/java/com/alfonso/capstone/fragments/MapsLocationFragment.java
@@ -2,9 +2,16 @@ package com.alfonso.capstone.fragments;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+
 import androidx.fragment.app.Fragment;
 
+import android.Manifest;
+
+import android.content.pm.PackageManager;
+
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,19 +19,28 @@ import android.view.ViewGroup;
 import com.alfonso.capstone.R;
 
 import com.alfonso.capstone.databinding.FragmentMapsLocationBinding;
+
+import com.alfonso.capstone.model.PlaceCapstone;
+import com.alfonso.capstone.services.imp.PlaceServiceGoogle;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.util.List;
 
 public class MapsLocationFragment extends Fragment {
 
     private GoogleMap map_;
     private FragmentMapsLocationBinding binding;
-
+    private final int CODE_RESULT_PERMISSION = 1;
+    private FusedLocationProviderClient locationProviderClient;
+    private PlaceServiceGoogle placeServiceGoogle;
 
     private final GoogleMap.OnInfoWindowClickListener windowClickListener = marker -> {
 
@@ -34,21 +50,33 @@ public class MapsLocationFragment extends Fragment {
         @Override
         public void onMapReady(GoogleMap googleMap) {
             map_ = googleMap;
-            LatLng sydney = new LatLng(-34, 151);
-            map_.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-            map_.moveCamera(CameraUpdateFactory.newLatLng(sydney));
             map_.setOnInfoWindowClickListener(windowClickListener);
+            map_.setMinZoomPreference(15.0f);
+            map_.setMaxZoomPreference(18.0f);
+            settingLocationEnabled();
         }
     };
-
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        binding = FragmentMapsLocationBinding.inflate(inflater,container,false);
+        binding = FragmentMapsLocationBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
+        permission();
+        locationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
+        placeServiceGoogle = new PlaceServiceGoogle(getContext());
+        placeServiceGoogle.getPlacesCurrentLocation().observe(this, this::addMarkers);
+        placeServiceGoogle.getPlaceById("ChIJ4a8nxjL50YURJ19RJ_A9T5E");
         return view;
     }
+
+    private void addMarkers(List<PlaceCapstone> places) {
+        places.forEach(placeCapstone -> {
+            Log.d("DATA",placeCapstone.toString());
+            map_.addMarker(new MarkerOptions().position(new LatLng(placeCapstone.getLatitude(),placeCapstone.getLongitude())).title(placeCapstone.getName()));
+        });
+    }
+
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -57,6 +85,40 @@ public class MapsLocationFragment extends Fragment {
                 (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
         if (mapFragment != null) {
             mapFragment.getMapAsync(callback);
+        }
+    }
+
+    private void permission() {
+        if (getContext().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            settingLocationEnabled();
+        } else {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, CODE_RESULT_PERMISSION);
+        }
+    }
+
+    public void settingLocationEnabled() {
+        if (getContext().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            if(map_ != null) {
+                map_.setMyLocationEnabled(true);
+                locationProviderClient.getLastLocation().addOnSuccessListener(location -> {
+                   if(location != null) {
+                       map_.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(),location.getLongitude()),15.0f));
+                   }
+                });
+            }
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == CODE_RESULT_PERMISSION) {
+            if(permissions.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                settingLocationEnabled();
+            } else {
+                AlertDialog alertDialog = new AlertDialog.Builder(getContext()).setMessage(getString(R.string.permission_reason)).setTitle(getString(R.string.permission_dialog_title))
+                        .setNeutralButton(R.string.ok, (dialogInterface, i) -> dialogInterface.cancel()).create();
+                alertDialog.show();
+            }
         }
     }
 }
