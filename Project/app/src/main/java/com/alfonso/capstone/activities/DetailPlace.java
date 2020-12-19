60b771d9f65545a707bd3750bb579b08b3d57b0f
Alfonso Balbuena Sánchez <alfonso1011@gmail.com>
2020-12-19 01:56:33 -0600

    Create DetailPlace.java

---
+++ Project/app/src/main/java/com/alfonso/capstone/activities/DetailPlace.java
@@ -0,0 +1,16 @@
+package com.alfonso.capstone.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.alfonso.capstone.R;
+
+public class DetailPlace extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_place);
+    }
+}
