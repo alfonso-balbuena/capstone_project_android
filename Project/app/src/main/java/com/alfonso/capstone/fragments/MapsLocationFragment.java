b4dc53a73cbc45a7801b259d3a096c93fce82d91
Alfonso Balbuena Sánchez <alfonso1011@gmail.com>
2020-12-20 23:52:17 -0600

    Add updating the location every time, after the update, the app update the closest places

--- Project/app/src/main/java/com/alfonso/capstone/fragments/MapsLocationFragment.java
+++ Project/app/src/main/java/com/alfonso/capstone/fragments/MapsLocationFragment.java
@@ -5,6 +5,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.Manifest;
 
@@ -12,11 +13,13 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 
 import android.os.Bundle;
+import android.os.Looper;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.alfonso.capstone.CapstoneApplication;
 import com.alfonso.capstone.R;
 
 import com.alfonso.capstone.activities.DetailPlace;
@@ -24,7 +27,13 @@ import com.alfonso.capstone.databinding.FragmentMapsLocationBinding;
 
 import com.alfonso.capstone.model.PlaceCapstone;
 import com.alfonso.capstone.services.imp.PlaceServiceGoogle;
+import com.alfonso.capstone.viewmodel.DetailPlaceViewModel;
+import com.alfonso.capstone.viewmodel.MapsLocationViewModel;
+import com.alfonso.capstone.viewmodel.factory.ViewModelRepositoryFactory;
 import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -40,13 +49,17 @@ public class MapsLocationFragment extends Fragment {
 
     private GoogleMap map_;
     private FragmentMapsLocationBinding binding;
+    private MapsLocationViewModel viewModel;
     private final int CODE_RESULT_PERMISSION = 1;
     private FusedLocationProviderClient locationProviderClient;
-    private PlaceServiceGoogle placeServiceGoogle;
+    private LocationRequest locationRequest;
+    private final long UPDATE_INTERVAL = 120000;
+    private LocationCallback locationCallback;
+    private boolean requestingLocationUpdates;
 
     private final GoogleMap.OnInfoWindowClickListener windowClickListener = marker -> {
         Intent intent = new Intent(requireActivity(), DetailPlace.class);
-        intent.putExtra(DetailPlace.PLACE_ID,marker.getTag().toString());
+        intent.putExtra(DetailPlace.PLACE_ID, marker.getTag().toString());
         startActivity(intent);
     };
 
@@ -66,17 +79,56 @@ public class MapsLocationFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         binding = FragmentMapsLocationBinding.inflate(inflater, container, false);
         View view = binding.getRoot();
+        viewModel = new ViewModelProvider(requireActivity(), new ViewModelRepositoryFactory<MapsLocationViewModel>(((CapstoneApplication) requireActivity().getApplication()).repository)).get(MapsLocationViewModel.class);
         permission();
-        locationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
-        placeServiceGoogle = new PlaceServiceGoogle(getContext());
-        placeServiceGoogle.getPlacesCurrentLocation().observe(this, this::addMarkers);
+        initLocationRequest();
+        viewModel.getPlacesCurrentPosition().observe(this, this::addMarkers);
         return view;
     }
 
     private void addMarkers(List<PlaceCapstone> places) {
         places.forEach(placeCapstone -> {
-            map_.addMarker(new MarkerOptions().position(new LatLng(placeCapstone.getLatitude(),placeCapstone.getLongitude())).title(placeCapstone.getName()).snippet(getString(R.string.click_info))).setTag(placeCapstone.getId());
+            map_.addMarker(new MarkerOptions().position(new LatLng(placeCapstone.getLatitude(), placeCapstone.getLongitude())).title(placeCapstone.getName()).snippet(getString(R.string.click_info))).setTag(placeCapstone.getId());
         });
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if(!requestingLocationUpdates) {
+            initUpdatesCurrentLocation();
+        }
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        locationProviderClient.removeLocationUpdates(locationCallback);
+        requestingLocationUpdates = false;
+    }
+
+    private void initUpdatesCurrentLocation() {
+        if (getContext().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+            locationCallback = new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    if (locationResult == null)
+                        return;
+                    map_.clear();
+                    viewModel.updatePlacesForPosition();
+                    map_.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(locationResult.getLastLocation().getLatitude(), locationResult.getLastLocation().getLongitude()), 18.0f));
+                }
+            };
+            requestingLocationUpdates = true;
+            locationProviderClient.requestLocationUpdates(locationRequest, locationCallback, Looper.getMainLooper());
+        }
+    }
+
+    private void initLocationRequest() {
+        locationProviderClient = LocationServices.getFusedLocationProviderClient(getContext());
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(UPDATE_INTERVAL);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
 
 
@@ -100,12 +152,12 @@ public class MapsLocationFragment extends Fragment {
 
     public void settingLocationEnabled() {
         if (getContext().checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-            if(map_ != null) {
+            if (map_ != null) {
                 map_.setMyLocationEnabled(true);
                 locationProviderClient.getLastLocation().addOnSuccessListener(location -> {
-                   if(location != null) {
-                       map_.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(),location.getLongitude()),15.0f));
-                   }
+                    if (location != null) {
+                        map_.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(location.getLatitude(), location.getLongitude()), 18.0f));
+                    }
                 });
             }
         }
@@ -114,7 +166,7 @@ public class MapsLocationFragment extends Fragment {
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         if (requestCode == CODE_RESULT_PERMISSION) {
-            if(permissions.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (permissions.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 settingLocationEnabled();
             } else {
                 AlertDialog alertDialog = new AlertDialog.Builder(getContext()).setMessage(getString(R.string.permission_reason)).setTitle(getString(R.string.permission_dialog_title))
--- Project/app/src/main/java/com/alfonso/capstone/viewmodel/MapsLocationViewModel.java
+++ Project/app/src/main/java/com/alfonso/capstone/viewmodel/MapsLocationViewModel.java
@@ -1,8 +1,12 @@
 package com.alfonso.capstone.viewmodel;
 
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.alfonso.capstone.model.PlaceCapstone;
 import com.alfonso.capstone.repository.IRepository;
+
+import java.util.List;
 
 public class MapsLocationViewModel extends ViewModel {
 
@@ -10,5 +14,14 @@ public class MapsLocationViewModel extends ViewModel {
 
     public MapsLocationViewModel(IRepository repository){
         this.repository = repository;
+
+    }
+
+    public LiveData<List<PlaceCapstone>> getPlacesCurrentPosition(){
+        return this.repository.getPlaceCurrentPosition();
+    }
+
+    public void updatePlacesForPosition() {
+        this.repository.updatePlacesCurrentPosition();
     }
 }
