0eac2b4456e90019303c86b89b2035bce8ea858f
Alfonso Balbuena Sánchez <alfonso1011@gmail.com>
2020-12-22 17:55:57 -0600

    Add toolbar to activities

--- Project/app/src/main/java/com/alfonso/capstone/activities/DetailPlace.java
+++ Project/app/src/main/java/com/alfonso/capstone/activities/DetailPlace.java
@@ -4,22 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.alfonso.capstone.CapstoneApplication;
 import com.alfonso.capstone.R;
+import com.alfonso.capstone.databinding.ActivityDetailPlaceBinding;
+import com.alfonso.capstone.databinding.ActivityMapsBinding;
 import com.alfonso.capstone.viewmodel.DetailPlaceViewModel;
 import com.alfonso.capstone.viewmodel.factory.ViewModelRepositoryFactory;
 
 public class DetailPlace extends AppCompatActivity {
 
     public final static String PLACE_ID = "PLACE_ID";
+    private ActivityDetailPlaceBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         DetailPlaceViewModel viewModel = new ViewModelProvider(this,new ViewModelRepositoryFactory<DetailPlaceViewModel>(((CapstoneApplication)getApplication()).repository)).get(DetailPlaceViewModel.class);
         viewModel.setIdPlace(getIntent().getStringExtra(PLACE_ID));
-        setContentView(R.layout.activity_detail_place);
+        binding = ActivityDetailPlaceBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setSupportActionBar(binding.myToolbar);
+        setContentView(view);
         setTitle(getString(R.string.detail));
     }
 }
--- Project/app/src/main/java/com/alfonso/capstone/activities/RouteActivity.java
+++ Project/app/src/main/java/com/alfonso/capstone/activities/RouteActivity.java
@@ -1,12 +1,15 @@
 package com.alfonso.capstone.activities;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.alfonso.capstone.CapstoneApplication;
 import com.alfonso.capstone.R;
+import com.alfonso.capstone.databinding.ActivityDetailPlaceBinding;
+import com.alfonso.capstone.databinding.ActivityRouteBinding;
 import com.alfonso.capstone.viewmodel.RouteViewModel;
 import com.alfonso.capstone.viewmodel.factory.ViewModelRepositoryFactory;
 
@@ -14,13 +17,17 @@ public class RouteActivity extends AppCompatActivity {
 
     public static final String ID_ROUTE = "ID_ROUTE";
     public static final String NAME_ROUTE = "NAME_ROUTE";
+    private ActivityRouteBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         RouteViewModel viewModel = new ViewModelProvider(this,new ViewModelRepositoryFactory<RouteViewModel>(((CapstoneApplication)getApplication()).repository)).get(RouteViewModel.class);
         viewModel.setIdRoute(getIntent().getLongExtra(ID_ROUTE,1));
+        binding = ActivityRouteBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setSupportActionBar(binding.myToolbar);
+        setContentView(view);
         setTitle(getIntent().getStringExtra(NAME_ROUTE));
-        setContentView(R.layout.activity_route);
     }
 }
