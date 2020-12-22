8cc4bb6e48dde478372a177307b755d6bbf6c240
Alfonso Balbuena Sánchez <alfonso1011@gmail.com>
2020-12-22 12:32:47 -0600

    Add signing configuration

--- Project/app/src/paid/java/com/alfonso/capstone/fragments/DetailPlaceFragment.java
+++ Project/app/src/paid/java/com/alfonso/capstone/fragments/DetailPlaceFragment.java
@@ -28,7 +28,6 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.squareup.picasso.Picasso;
 
 public class DetailPlaceFragment extends Fragment implements OnMapReadyCallback, AdapterView.OnItemSelectedListener {
 
